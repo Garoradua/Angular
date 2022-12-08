592b43ffe80b388f1782142d4978aa2a87a7dea9
Gourav Arora <gouravarora035@gmail.com>
2022-12-08 17:19:55 +0530

    new app

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DataComponent} from './data/data.component';
+import { AboutComponent } from './about/about.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:'data',component:DataComponent},
+  {path:"about", component: AboutComponent},
+  {path:"about/aboutme", component: AboutMeComponent},
+  {path:"about/aboutcompany", component: AboutCompanyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import {FormsModule, FormGroup, FormControl} from '@angular/forms'
 import {ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DataComponent } from './data/data.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DataComponent,
+    AboutCompanyComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
